18a172ee2dfdd80070ff44146c04471395b86c79
rams-mars <ranggajs.st@gmail.com>
2025-08-03 03:13:59 +0700

    fix: disable strict eslint rules for deployment

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -10,7 +10,13 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended"
+  ),
 ];
 
 export default eslintConfig;
+
